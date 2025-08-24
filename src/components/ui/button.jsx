1cca77d4461dcabb5a0a0ea74c5efbae9609b819
devyjones991-ai <devyjones991@gmail.com>
2025-08-24 22:50:37 +0300

    test: add Button component tests

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import PropTypes from 'prop-types'
 
@@ -64,35 +63,10 @@ Button.displayName = 'Button'
 
 Button.propTypes = {
   className: PropTypes.string,
-  variant: PropTypes.string,
-  size: PropTypes.string,
+  variant: PropTypes.oneOf(Object.keys(variants)),
+  size: PropTypes.oneOf(Object.keys(sizes)),
   asChild: PropTypes.bool,
   children: PropTypes.node,
 }
 
 export { Button }
-
-import PropTypes from 'prop-types'
-
-export function Button({
-  size = 'default',
-  variant = 'primary',
-  className = '',
-  ...props
-}) {
-  const sizeClass = size === 'sm' ? 'btn-sm' : size === 'lg' ? 'btn-lg' : ''
-  const variantClass = variant ? `btn-${variant}` : ''
-  const combined = ['btn', sizeClass, variantClass, className]
-    .filter(Boolean)
-    .join(' ')
-  return <button className={combined} {...props} />
-}
-
-Button.propTypes = {
-  size: PropTypes.oneOf(['sm', 'lg', 'default']),
-  variant: PropTypes.string,
-  className: PropTypes.string,
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func,
-}
-
